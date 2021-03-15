7513b89a2c7737ac7880421e84a886cf665e07bd
pix666 <pix666@ya.ru>
2021-03-15 11:55:33 +0400

    Bump to v0.3.1

--- jinja2_simple_tags.py
+++ jinja2_simple_tags.py
@@ -2,7 +2,7 @@ from jinja2 import nodes
 from jinja2.ext import Extension
 
 __all__ = ['StandaloneTag', 'ContainerTag']
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 
 class BaseTemplateTag(Extension):
